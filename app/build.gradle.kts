5a2f800052bcb6b98c67bd312fc4e67e8d06faff
salamandermartin <salamancam.programming@gmail.com>
2024-09-22 12:33:37 -0400

    dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.tensorflow.lite)
+    implementation(libs.tensorflow.lite.support)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
